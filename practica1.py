37a294d1fbede49bbd25de46b3ac95e466cd7049
Drake Nain Jara Sevilla <Drakenain24@gmail.com>
2026-05-03 18:48:13 -0600

    Selfs

--- practica1.py
+++ practica1.py
@@ -1,2 +1,7 @@
 class Persona:
     def _init_(self, nombre, edad, altura, peso, ciudad):
+        self.__nombre = nombre
+        self.__edad = edad
+        self.__altura = altura
+        self.__peso = peso
+        self.__ciudad = ciudad
